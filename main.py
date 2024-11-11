7b2402e5d58b256f714938b0a72a1a14450e3b12
wl9pa <wl9pa-92-92@mail.ru>
2024-11-11 18:04:01 +0300

    Добавлены хэндлеры на команду /start и на команду /help.

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+import asyncio
 from aiogram import Bot, Dispatcher
-from config_data.config import load_config
+from config_data.config import load_config, Config
 
 
-config = load_config(r'E:\modular_echo_bot\.env')
+async def main() -> None:
+    config: Config = load_config()
+
+    bot = Bot(token=config.tg_bot.token)
+    dp = Dispatcher()
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+asyncio.run(main())

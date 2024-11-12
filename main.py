5f8ab43476ebfab868b6361254af640578852c8c
wl9pa <wl9pa-92-92@mail.ru>
2024-11-12 19:03:26 +0300

    Добавлены роутеры

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import asyncio
+
 from aiogram import Bot, Dispatcher
 from config_data.config import load_config, Config
+from handlers import other_handlers, user_handlers
 
 
+# Функция конфигурирования и запуска бота
 async def main() -> None:
+    # Загружаем конфиг в переменную config
     config: Config = load_config()
 
+    # Инициализируем бот и диспетчер
     bot = Bot(token=config.tg_bot.token)
     dp = Dispatcher()
+
+    dp.include_router(user_handlers.router)
+    dp.include_router(other_handlers.router)
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
